e487474b52b3c9c59650a37ea276648755160c78
Daniel Araya <darayaroma@gmail.com>
2023-05-20 01:56:22 -0600

    [hello world] using cmake

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # karnaugh-map-solver
 A program that can solve 4 variable k-maps, then 5-variables, also don't cares.
+
+## Building
+I'm learning to use cmake to build cpp programs
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/araya/Repositories/karnaugh-map-solver
+# Build directory: /home/araya/Repositories/karnaugh-map-solver/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
